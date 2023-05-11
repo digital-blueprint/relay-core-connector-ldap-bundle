851f86186efbdfa1dce2d0b34db37e99361e7ea0
Christoph Reiter <reiter.christoph@gmail.com>
2023-05-11 14:08:38 +0200

    Update to api-platform 2.7

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -50,5 +50,9 @@ class Kernel extends BaseKernel
         ]);
 
         $container->extension('dbp_relay_core_connector_ldap', []);
+
+        $container->extension('api_platform', [
+            'metadata_backward_compatibility_layer' => false,
+        ]);
     }
 }
